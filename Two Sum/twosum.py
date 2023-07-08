a5996df5d56450215419e9b6fed0f2a2feac3c08
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2023-07-08 14:44:31 +0500

    Added Solution of Two Sum Problem in Python

---
+++ Two Sum/twosum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums, target):
+        numtoindex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numtoindex:
+                return [numtoindex[target - nums[i]], i]
+            numtoindex[nums[i]] = i
